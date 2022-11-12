80daae05c11c3a389cf1cee32b451036639c6910
mlacker <mlcker@163.com>
2022-11-12 16:19:14 +0800

    remove passwall & netdata

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,32 +10,20 @@
 sed -i 's/192.168.1.1/192.168.10.250/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
-
+sed -i 's/luci-theme-bootstrap/luci-theme-neobird/g' ./feeds/luci/collections/luci/Makefile
+sed -i 's/shadowsocksr/vssr/g' ./package/lean/luci-theme-neobird/luasrc/view/themes/neobird/header.htm
 
 # Package
-
 git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
 git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
 
-# rm -rf package/helloworld
-# git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
-# svn co https://github.com/fw876/helloworld/trunk/xray-core package/helloworld/xray-core
-# svn co https://github.com/fw876/helloworld/trunk/xray-plugin package/helloworld/xray-plugin
-# svn co https://github.com/fw876/helloworld/trunk/trojan package/helloworld/trojan
-# svn co https://github.com/fw876/helloworld/trunk/simple-obfs package/helloworld/simple-obfs
-# svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/helloworld/luci-app-ssr-plus
-
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/openwrt-passwall2
-
-# svn co https://github.com/kenzok8/small-package/trunk/luci-app-alist package/luci-app-alist
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/openwrt-passwall2
 
 # svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
 
 
 # Themes
-
 # luci-theme-argon
 rm -rf package/lean/luci-theme-argon
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
